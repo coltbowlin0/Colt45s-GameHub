d7e10a86adab8456d3cfd39c7019d73b48e52a8a
Colt Bowlin <coltbowlin0@gmail.com>
2026-09-15 20:30:19 -0400

    among-us added, updated tsa game

---
+++ games/amongus/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "XeraGamesLLC-Among Us-2019.2.15";
+const contentToCache = [
+    "Build/workywork.loader.js",
+    "Build/workywork.framework.js",
+    "Build/workywork.data",
+    "Build/workywork.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
